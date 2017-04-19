78d2b35e6c99a537569f31438e218d19bed2800b
Paul Breaux <p.breaux@f5.com>
2017-04-19 10:29:15 -0600

    Newton process creation event changed from mitaka
    Issues:
    Fixes #528
    
    Problem:
    When a neutron worker is created in mitaka, the AFTER_CREATE event is
    emitted, yet when the worker is created in newton, the AFTER_INIT event
    is emitted. Now that we wait for these processes to be forked before we
    create the RPC listener in the driver, we must rely upon the proper
    event before agent and driver communication can commence.
    
    Analysis:
    Changed the AFTER_CREATE event to AFTER_INIT and communication then
    flowed between driver and agent.
    
    Tests:
    Ran manual tests for creation of loadbalancer, then automated testing
    with all tests.

--- systest/scripts/install_test_infra.sh
+++ systest/scripts/install_test_infra.sh
@@ -35,7 +35,7 @@ pip install ${TEMPEST_DIR}
 rm -rf ${DEVTEST_DIR}
 git clone -b ${TEST_OPENSTACK_DISTRO} ${DEVTEST_REPO} ${DEVTEST_DIR}
 
-pip install git+ssh://git@bldr-git.int.lineratesystems.com/tools/pytest-autolog.git
+pip install git+https://gitlab.pdbld.f5net.com/tools/pytest-autolog.git
 # This should be listed in requirement.test.txt also, but will not succeed
 # from that location without sudo
 sudo pip install git+https://github.com/F5Networks/f5-openstack-agent.git@${BRANCH}
