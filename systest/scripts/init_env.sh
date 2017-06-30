bc81140ca54761246e67c86cfe922cffda2d6d95
Paul Breaux <p.breaux@f5.com>
2017-06-30 11:15:27 -0600

    Changing ci results dir to local /tmp directory.

--- systest/scripts/init_env.sh
+++ systest/scripts/init_env.sh
@@ -10,7 +10,8 @@ export CI_BRANCH=$(echo $JOB_NAME | cut -d "/" -f 3)
 
 job_dirname="${CI_PROGRAM}.${CI_PROJECT}.${CI_BRANCH}.${JOB_BASE_NAME}"
 build_dirname="${JOB_BASE_NAME}-${BUILD_ID}"
-export CI_RESULTS_DIR="/home/jenkins/results/${job_dirname}/${build_dirname}"
+#export CI_RESULTS_DIR="/home/jenkins/results/${job_dirname}/${build_dirname}"
+export CI_RESULTS_DIR="/tmp/jenkins/results/${job_dirname}/${build_dirname}"
 export CI_BUILD_SUMMARY="${CI_RESULTS_DIR}/ci-build.yaml"
 
 # - source this job's environment variables
